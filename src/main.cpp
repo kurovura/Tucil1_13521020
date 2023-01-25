4825f791b34c8cc1643820f862c5b04ace5a178e
varrazha <106954599+varrazha@users.noreply.github.com>
2023-01-25 12:52:29 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,179 @@
 #include<bits/stdc++.h>
-#include "validator.cpp"
-#include "olahInput.cpp"
-#include "mathInput.cpp"
+#include <chrono>
 using namespace std;
 
 
-int n;
+int n,i,j,k ;
+int kartu[4];
+string c1,c2,c3,c4 ;
+int totalSolusi = 0;
+string hasilOpr[99999] = {} ;
+
+int strInt (string str){
+    int nilai ;
+    if (str == "A"){
+        nilai = 1;
+    } else if (str == "2") {
+        nilai = 2;
+    } else if (str == "3") {
+        nilai = 3;
+    } else if (str == "4") {
+        nilai = 4;
+    } else if (str == "5"){
+        nilai = 5;
+    } else if (str == "6") {
+        nilai = 6;
+    } else if (str == "7") {
+        nilai = 7;
+    } else if (str == "8") {
+        nilai = 8;
+    } else if (str == "9"){
+        nilai = 9;
+    } else if (str == "10") {
+        nilai  = 10;
+    } else if (str == "J") {
+        nilai = 11;
+    } else if (str == "Q") {
+        nilai = 12;
+    }else if (str == "K"){
+        nilai = 13;
+    }
+    return nilai;
+}
+
+string intkartu(double nilai){
+    string remi;
+    if (nilai == 1){
+        remi == "A" ;
+    }
+    else if (nilai == 2){
+        remi == "2" ;
+    }
+    else if (nilai == 3){
+        remi == "3" ;
+    }
+    else if (nilai == 4){
+        remi == "4" ;
+    }
+    else if (nilai ==  5){
+        remi == "5" ;
+    }
+    else if (nilai == 6){
+        remi == "6" ;
+    }
+    else if (nilai == 7){
+        remi == "7" ;
+    }
+    else if (nilai == 8){
+        remi == "8" ;
+    }
+    else if (nilai == 9){
+        remi == "9" ;
+    }
+    else if (nilai == 10){
+        remi == "10" ;
+    }
+    else if (nilai == 11){
+        remi == "J" ;
+    }
+    else if (nilai == 12){
+        remi == "Q" ;
+    }
+    else if (nilai == 13){
+        remi == "K" ;
+    }
+    return remi ;
+}
+
+void intTandaStr(int angkaTanda, string *simbol){
+    if(angkaTanda == 0) {*simbol = "+" ;}
+    else if(angkaTanda == 1) {*simbol = "-" ;}
+    else if(angkaTanda == 2) {*simbol = "*"; }
+    else if(angkaTanda == 3) {*simbol = "/"; }
+}
+
+
+double perhitungan (int a, int b, int tanda){
+    int hasil;
+    switch (tanda){
+        case 0 :
+            hasil = a+b ;
+        case 1 :
+            hasil = a-b ;
+        case 2 :
+            hasil = a*b ;
+        case 3 : 
+            if (b!=0){
+                hasil =  a/b ;
+            } 
+            else{ //b=0
+                hasil = 999999999;
+            }
+    }
+    return hasil;
+}
+   
+// ((ab)c)d
+double kalkulasi1 (int input[4], int op1, int op2, int op3){
+    double hasil ;
+    hasil = perhitungan(input[0], input[1], op1) ;
+    hasil = perhitungan(hasil, input[2], op2) ;
+    hasil = perhitungan(hasil, input[3], op3) ;
+    return hasil;
+}
+
+// (ab)(cd)
+double kalkulasi2 (int input[4], int op1, int op2, int op3){
+    double hasil1, hasil2, hasil ;
+    hasil1 = perhitungan(input[0], input[1], op1) ;
+    hasil2 = perhitungan(input[2], input[3], op3) ;
+    hasil = perhitungan(hasil1, hasil2, op2) ;
+    return hasil;
+}
+
+// (a(bc))d
+double kalkulasi3 (int input[4], int op1, int op2, int op3){
+    double hasil ;
+    hasil = perhitungan(input[1], input[2], op2) ;
+    hasil  = perhitungan(input[0], hasil, op1) ;
+    hasil = perhitungan(hasil, input[3], op3) ;
+    return hasil;
+}
+
+// a((bc)d)
+double kalkulasi4 (int input[4], int op1, int op2, int op3){
+    double hasil ;
+    hasil = perhitungan(input[1], input[2], op2) ;
+    hasil = perhitungan(hasil, input[3], op3) ;
+    hasil = perhitungan(input[0], hasil, op1) ;
+    return hasil;
+}
+
+// a(b(cd))
+double kalkulasi5 (int input[4], int op1, int op2, int op3){
+    double hasil ;
+    hasil = perhitungan(input[2], input[3], op3) ;
+    hasil = perhitungan(input[1], hasil, op2) ;
+    hasil = perhitungan(input[0], hasil, op1) ;
+    return hasil;
+}
+
+bool inputMenuValidator(int n){
+    return (n==1||n==2||n==3);
+}
+
+int validInp (string typing){
+    
+    if (typing.length() == 1 ){
+        return (typing[0] == 'A' ||typing[0] == '2'||typing[0] == '3'||typing[0] == '4'||typing[0] == '5'||typing[0] == '6'||typing[0] == '7'||typing[0] == '8'||typing[0] == '9'||typing[0] == 'J'||typing[0] == 'Q'||typing[0] == 'K');
+    }
+    else if (typing.length()== 2){
+        return (typing[0]=='1' && typing[1]== '0');
+    }
+    else{
+        return false;
+    }
+}
 
 void menu24(){
         cout << "Menu :"<<endl;
@@ -14,56 +182,84 @@ void menu24(){
         cout << "3. Keluar"<<endl;
 }
 
+
+
 int main(){
     while(true){
         cout << "24 Game"<<endl;
-        cout << "Menu :"<<endl;
-        cout << "1. Input 4 kartu"<<endl;
-        cout << "2. Generate 4 kartu secara random"<<endl;
-        cout << "3. Keluar"<<endl;
+        menu24();
         cout << "Masukkan pilihan :"<<" " ;
         cin>> n;
-
 
         while(!inputMenuValidator(n)){
             cout << "Input tidak valid. Masukkan lagi sesuai menu!"<<endl;
             cin>> n;
         }
 
-        if (n==1 || n==2){
-            if (n==1){
-                cout << "Input 4 kartu ya!"<<  endl ;
-                string c1,c2,c3,c4 ;
-                cin>> c1>>c2>>c3>>c4 ;
-                while (!validInp(c1)||!validInp(c2)||!validInp(c3)||!validInp(c4)){
-                    cout<< "Masukan unvalid. Silakan input lagi!" ;
-                    cin>> c1>>c2>>c3>>c4 ;
+        if(n==1 or n==2){
+            if(n==1){
+                cout << "Masukkan 4 kartu ! ";
+                cin >> c1 >> c2 >> c3 >> c4;
+                while(!validInp(c1) || !validInp(c2) || !validInp(c3) || !validInp(c4)){
+                    cout << "Input tidak valid, masukkan lagi!"<< endl ;
+                    cin >> c1 >> c2 >> c3 >> c4;
                 }
-                kartu[1] == strInt(c1); kartu[2] == strInt(c2); kartu[3] == strInt(c3); kartu[4] == strInt(c4);
+                kartu[0] = strInt(c1);kartu[1] = strInt(c2);kartu[2] = strInt(c3);kartu[3] = strInt(c4);
             }
             else {
-                int i;
-                cout << "4 kartu terpilih : " << endl ;
-                for (i=0; i<=3; i++){
-                    kartu[i] = rand() % 13+1 ;
+                for(int i=0; i<4; i++){
+                    kartu[i] = rand() % 13 + 1;
                 }
-                
-                for (i=0; i<=3; i++){
-                    intkartu(kartu[i]);
-                    cout << kartu[i];
+                cout << "Kartu yang terpilih :";
+                for(int i=0; i<4; i++){
+                    cout<< intkartu(kartu[i]);
                 }
-                cout << endl; 
-            }
-
-            int totalSolusi = 0;
-            string hasilOpr[99999] = {} ;
-            sort(kartu, kartu+4);
+                cout << endl;   }   
             
-            auto start = chrono::steady_clock::now();
-            tampungHasil24();
+            
+            sort(kartu, kartu+4);
+            auto mulai = chrono::steady_clock::now();
+            do {
+                int hasil;
+                for (i=0; i=3; i++){
+                    for (j=0; j=3; j++){
+                        for (k=0; k=3; k++){
+                            string op1, op2, op3 ;
+                            intTandaStr(i, &op1);  intTandaStr(j, &op2);  intTandaStr(k, &op3);
+                            
+                            // ((ab)c)d
+                            if (kalkulasi1( kartu, i,  j, k)== 24){
+                                totalSolusi ++ ;
+                                hasilOpr[totalSolusi] = "((" + intkartu(kartu[0]) + " " + op1 + " " + intkartu(kartu[1]) + ") " + op2 + " " + intkartu(kartu[2]) + ") " + op3 + " " + intkartu(kartu[3]);
+                            }
+                            // (ab)(cd)
+                            if (kalkulasi2( kartu, i,  j, k)== 24){
+                                totalSolusi ++ ;
+                                hasilOpr[totalSolusi] = "(" + intkartu(kartu[0]) + " " + op1 + " " + intkartu(kartu[1]) + ") " + op2 + " (" + intkartu(kartu[2]) + " " + op3 + " " + intkartu(kartu[3]) + ")";
+                            }
+                            // (a(bc))d
+                            if (kalkulasi3( kartu, i,  j, k)== 24){
+                                totalSolusi ++ ;
+                                hasilOpr[totalSolusi] =   "(" + intkartu(kartu[0]) + " " + op1 + " (" + intkartu(kartu[1]) + " " + op2 + " " + intkartu(kartu[2]) + ")) " + op3 + " " + intkartu(kartu[3]) ;
+                            }
+                            // a((bc)d)
+                            if (kalkulasi4( kartu, i,  j, k)== 24){
+                                totalSolusi ++ ;
+                                hasilOpr[totalSolusi] = intkartu(kartu[0]) + " " + op1 + " ((" + intkartu(kartu[1]) + " " + op2 + " " + intkartu(kartu[2]) + ") " + op3 + " " + intkartu(kartu[3]) + ")" ;
+                            }
+                            // a(b(cd))
+                            if (kalkulasi5( kartu, i,  j, k)== 24){
+                                totalSolusi ++ ;
+                                hasilOpr[totalSolusi] = intkartu(kartu[0]) + " " + op1 + " " + "(" + intkartu(kartu[1]) + " " + op2 + " " + "("+ intkartu(kartu[2]) + " " + op2 + " " + intkartu(kartu[3]) + "))" ;
+                            }
+                        }
+                    }
+                }
+            }while (next_permutation(kartu, kartu+4));
+
         
-            auto end = chrono::steady_clock::now();
-            auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+            auto selesai = chrono::steady_clock::now();
+            auto durasi = chrono::duration_cast<chrono::nanoseconds>(selesai - mulai);
 
             if (totalSolusi == 0){
                 cout<< "Solusi nihil" ;
@@ -74,18 +270,18 @@ int main(){
                 for (i = 1 ; i = totalSolusi; i++){
                     cout << hasilOpr[i]<< endl ;
                 }
-                cout << "Waktu penyelesaian = " <<  duration.count() << " " << endl ;
+                cout << "Waktu penyelesaian = " <<  durasi.count() << " " << "nanosekon"<<endl ;
             }   
             
-            cout << "Apakah anda ingin menyimpan hasil pencarian ke dalam file? (y/n) ";
-            char c;
-            cin >> c;
-            while(c != 'y' and c != 'n'){
-                cout << "Input tidak valid\n";
-                cout << "Apakah anda ingin menyimpan hasil pencarian ke dalam file? (y/n) ";
-                cin >> c;
+            cout << "Apakah anda ingin menyimpan hasil pencarian ke dalam file?"<<endl;
+            cout << "masukkan y atau n!"<<endl ;
+            char yn;
+            cin >> yn;
+            while(yn != 'y' && yn != 'n'){
+                cout << "Input tidak valid, masukkan lagi input!"<<endl;
+                cin >> yn;
             }
-            if(c == 'y'){
+            if(yn == 'y'){
                 ofstream file;
                 string namafile;
                 cout << "Masukkan nama file: ";
@@ -94,23 +290,9 @@ int main(){
                 file.open(namafile);
                 file << "Kartu yang terpilih :";
                 for(int i=0; i<4; i++){
-                    if(kartu[i] == 1){
-                        file << " A";
-                    }
-                    else if(kartu[i] == 11){
-                        file << " J";
-                    }
-                    else if(kartu[i] == 12){
-                        file << " Q";
-                    }
-                    else if(kartu[i] == 13){
-                        file << " K";
-                    }
-                    else{
-                        file << " " << kartu[i];
-                    }
+                    file << intkartu(kartu[i]);
                 }
-                file << "\n";
+                file << endl;
                 if(totalSolusi == 0){
                     file << "Solusi nihil"<< endl;
                 }
@@ -121,30 +303,34 @@ int main(){
                         file << hasilOpr[i] << endl;
                     }
                 }
-                file << "Waktu penyelesaian = " <<  duration.count() << " " << endl ;
+                file << "Waktu penyelesaian = " <<  durasi.count() << " " << endl ;
                 file.close();
             }
             else{
-                cout << "Hasil pencarian tidak disimpan\n";
+                cout << "Hasil pencarian tidak disimpan"<<endl;
             }
-            cout << "Apakah anda ingin melanjutkan permainan? (y/n) ";
-            char c2;
-            cin >> c2;
-            while(c2 != 'y' and c2 != 'n'){
-                cout << "Input tidak valid\n";
-                cout << "Apakah anda ingin melanjutkan permainan? (y/n) ";
-                cin >> c2;
+            cout << "Apakah anda ingin melanjutkan permainan?"<<endl;
+            cout << "y atau n?"<<endl;
+            char yn2;
+            cin >> yn2;
+            while(yn2 != 'y' && yn2 != 'n'){
+                cout << "Input tidak valid. Input lagi!"<<endl;
+                cin >> yn2;
             }
-            if(c2 == 'n'){
+            if (yn=='y'){
+                main();
+            }
+            if(yn2 == 'n'){
                 cout << "Sip. Terima kasih sudah bermain, ya!";
-                return 0;
             }
         }
+    
         else if(n == 3){
-            cout << "Terima kasih telah bermain\n";
+            cout << "Sip!";
         }
+
+        return 0;
     }
-    return 0;
 }
     
 
